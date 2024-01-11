9a11f739ec6fc74637bb3b7220842f5b6c8bbb68
LaPatusek <ddrrusek@gmail.com>
2024-01-11 11:47:55 +0100

    google docs

--- src/Pages/Regulamin.tsx
+++ src/Pages/Regulamin.tsx
@@ -2,35 +2,14 @@ import React from 'react';
 import styles from './Regulamin.module.css';
 
 const Regulamin: React.FC = () => {
+  const documentURL =
+    'https://docs.google.com/document/d/e/2PACX-1vQfiS4FYZaHab93z35dU9Hqzn50CFO1DOE7Gm3k2uXGqBszZoSAPLKqlkuoLWOD6gxkMJqrsh47OOIa/pub?embedded=true';
+
   return (
     <div className={styles['regulamin-wrap']}>
       <h1>REGULAMIN SERWERA CITY OF LIGHTS</h1>
       <div className={styles.regulamin}>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-          venenatis luctus ipsum. Nam quis fringilla lacus, sit amet sagittis
-          ligula. Vestibulum ullamcorper tortor vitae quam ullamcorper
-          ultricies. Nam vel malesuada lorem. Cras nisl ligula, semper id nisl
-          non, placerat tempor nulla. Maecenas eget viverra sem, vel lacinia
-          lorem. Vivamus vestibulum magna ipsum, eu tincidunt turpis porta nec.
-          Vestibulum quis sem et ipsum facilisis maximus fringilla sed enim. Ut
-          risus urna, finibus vitae leo eget, posuere mattis elit. Aenean et
-          purus rutrum, tincidunt nibh sed, accumsan lorem. Nulla vulputate
-          hendrerit diam, eu semper est posuere vel. Sed leo ipsum, vehicula ac
-          libero id, sagittis pellentesque ipsum. Interdum et malesuada fames ac
-          ante ipsum primis in faucibus. Nullam a massa dui. Sed pharetra mi non
-          enim volutpat consequat. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Donec dictum augue id eros efficitur, vitae interdum
-          ligula faucibus. Etiam augue mauris, mattis eu fringilla vitae,
-          iaculis vestibulum lectus. Phasellus nec tortor eu Nulla vulputate
-          hendrerit diam, eu semper est posuere vel. Sed leo ipsum, vehicula ac
-          libero id, sagittis pellentesque ipsum. Interdum et malesuada fames ac
-          ante ipsum primis in faucibus. Nullam a massa dui. Sed pharetra mi non
-          enim volutpat consequat. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Donec dictum augue id eros efficitur, vitae interdum
-          ligula faucibus. Etiam augue mauris, mattis eu fringilla vitae,
-          iaculis vestibulum lectus. Phasellus nec tortor eu
-        </p>
+        <iframe src={documentURL} title='Regulamin Serwera City of Lights' />
       </div>
     </div>
   );
